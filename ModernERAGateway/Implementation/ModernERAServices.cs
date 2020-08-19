0883a961c3cc8e3c1bb18b8f5ded956fa3c7af9a
Sonakshi Rastogi <sonakshirastogi75@gmail.com>
2020-08-19 12:45:14 -0700

    Added project

---
+++ ModernERAGateway/Implementation/ModernERAServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernERAGateway.Implementation
+{
+    public class ModernERAServices
+    {
+    }
+}
